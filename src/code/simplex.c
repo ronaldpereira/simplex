a63114db68fde2de36a4403e9dfa0e6cd9dab43a
Ronald Pereira <ronald.drp11@gmail.com>
2017-04-22 13:36:52 -0300

    Added Makefile and IO for source code

--- src/code/simplex.c
+++ src/code/simplex.c
@@ -4,7 +4,12 @@
 
 int main(int argc, char* argv[])
 {
-    /* code */
+    FILE *entrada, *saida;
+    entrada = fopen(argv[1], "r");
+    saida = fopen(argv[2], "w");
+
+    fclose(entrada);
+    fclose(saida);
 
     return 0;
 }
