627220bb6c3b1335ae14eba34864244f8642d91e
Ronald Pereira <ronald.drp11@gmail.com>
2017-04-27 21:36:32 -0300

    Matrix allocation and disallocation functions finished and print matrix function ready

--- src/code/simplex.c
+++ src/code/simplex.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void dimensionCounter(FILE *input, int *lines, int *columns)
+void dimensionCounter(FILE **input, int *lines, int *columns)
 {
     char c;
     int flag = 0;
     *lines = -1;
     *columns = 1;
 
-    while(fscanf(input, "%c", &c) != EOF)
+    while(fscanf((*input), "%c", &c) != EOF)
     {
         if(c == '{')
             (*lines)++;
@@ -18,7 +18,7 @@ void dimensionCounter(FILE *input, int *lines, int *columns)
         {
             while(1)
             {
-                fscanf(input, "%c", &c);
+                fscanf((*input), "%c", &c);
 
                 if(c == '}')
                 {
@@ -28,9 +28,45 @@ void dimensionCounter(FILE *input, int *lines, int *columns)
 
                 if(c == ',')
                     (*columns)++;
-
             }
         }
+    }
+}
+
+double **matrixAllocation(int *lines, int *columns)
+{
+    int i;
+    double **matrix;
+
+    matrix = (double**) calloc(*lines,sizeof(double*));
+    for(i = 0; i < *lines; i++)
+        matrix[i] = (double*) calloc(*columns, sizeof(double*));
+
+    return matrix;
+}
+
+double **matrixDisallocation(double **matrix, int *lines, int *columns)
+{
+    int i;
+
+    for(i = 0; i < *lines; i++)
+        free(matrix[i]);
+    free(matrix);
+
+    return NULL;
+}
+
+void printMatrix(double **matrix, int lines, int columns)
+{
+    int i, j;
+
+    for(i = 0; i < lines; i++)
+    {
+        for(j = 0; j < columns; j++)
+        {
+            printf("%.5lf ", matrix[i][j]);
+        }
+        printf("\n");
     }
 }
 
@@ -38,13 +74,20 @@ int main(int argc, char* argv[])
 {
     FILE *input, *output;
     int lines, columns;
+    double **matrix;
 
     input = fopen(argv[1], "r");
     output = fopen(argv[2], "w");
 
-    dimensionCounter(input, &lines, &columns);
+    dimensionCounter(&input, &lines, &columns);
+    //TODO printftest
     printf("%d X %d\n", lines, columns);
 
+    matrix = matrixAllocation(&lines, &columns);
+
+    printMatrix(matrix, lines, columns);
+
+    matrixDisallocation(matrix, &lines, &columns);
     fclose(input);
     fclose(output);
 
